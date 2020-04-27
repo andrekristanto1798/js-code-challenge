fc6b07880b69c14f29ae8d7e7f962b1dad482ab1
Andre Kristanto <akristan001@e.ntu.edu.sg>
2020-04-27 10:00:21 +0800

    Add three ways to sum to N

--- src/three_ways_to_sum_to_n/solution_b.js
+++ src/three_ways_to_sum_to_n/solution_b.js
@@ -3,6 +3,16 @@
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
-var sum_to_n = function(n) {
-    // your code here
+// Manually add from 1 until N using for loop
+// Time complexity: O(N)
+// Space complexity: O(1)
+var sum_to_n = function (n) {
+  if (n <= 0) return 0;
+  let sum = 0;
+  for (let i = 1; i <= n; i += 1) {
+    sum += i;
+  }
+  return sum;
 };
+
+console.log(sum_to_n(5));
--- src/three_ways_to_sum_to_n/solution_c.js
+++ src/three_ways_to_sum_to_n/solution_c.js
@@ -3,6 +3,18 @@
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
-var sum_to_n = function(n) {
-    // your code here
+// Manually add from 1 until N using while loop
+// Time complexity: O(N)
+// Space complexity: O(1)
+var sum_to_n = function (n) {
+  if (n <= 0) return 0;
+  let i = 1;
+  let sum = 0;
+  while (i <= n) {
+    sum += i;
+    i += 1;
+  }
+  return sum;
 };
+
+console.log(sum_to_n(5));
